b76cb5e2c27f284b848d3f3b650890b006cc8b89
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2015-06-14 10:27:14 +0200

    feat(*): fix nom de la formation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,15 @@
 module.exports = function(grunt) {
 
+    grunt.initConfig({
+        zenika: {
+            formation: {
+                name: 'formation-html5'
+            }
+        }
+    });
+
     grunt.loadTasks('node_modules/zenika-formation-framework');
-	
+
     grunt.registerTask('default', ['displaySlides']);
 
 };
